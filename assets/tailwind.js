a720b338f838263b4f169f74eb185d6103a64405
khushboo-590 <khushbook4979@gmail.com>
2025-06-16 07:17:40 +0530

    hero section

---
+++ assets/tailwind.js
@@ -0,0 +1,14 @@
+
+tailwind.config = {
+    theme: {
+            colors: {
+                "light-blue": "#1F2937",
+                "red": "#F94600",
+            "gray": "#969696",
+            "light-gray":"#8080800D",
+                "white": "#ffffff",
+                "black": "#000000",
+            },
+        },
+    }
+
