b69effc3f09831c75f71958f9a02613aea0f24d8
khushboo-590 <khushbook4979@gmail.com>
2025-06-16 12:54:20 +0530

    add margin in hero section

--- assets/tailwind.js
+++ assets/tailwind.js
@@ -1,15 +1,15 @@
 
 tailwind.config = {
     theme: {
-            colors: {
-                "light-blue": "#1F2937",
-                "red": "#F94600",
+        colors: {
+            "light-blue": "#1F2937",
+            "red": "#F94600",
             "gray": "#969696",
             "light-gray": "#8080800D",
-            "dark-blue":"#374151",
-                "white": "#ffffff",
-                "black": "#000000",
-            },
+            "dark-blue": "#374151",
+            "white": "#ffffff",
+            "black": "#000000",
         },
-    }
+    },
+}
 
